72c41b43330e6b1140dcf6a9c7249ceb96966e3a
Marty Hernandez Avedon <martyavedon@gmail.com>
2018-11-27 02:48:16 -0500

    height and weight

--- whos_that_pokemon.py
+++ whos_that_pokemon.py
@@ -13,12 +13,26 @@ import pokebase as pb
 PARSER = argparse.ArgumentParser()
 PARSER.add_argument('name', help='The name of the Pokemon you want information on.')
 ARGS = PARSER.parse_args()
+
 QUICK_LOOK_UP = pb.pokemon(ARGS.name)
-TEMPLATE = Template('$name, a very special Pokemon.')
-FORMATTED = TEMPLATE.substitute(name=QUICK_LOOK_UP.species[0].upper() + QUICK_LOOK_UP.species[1:])
+NAME_AND_VITALS = Template('$name, a very special Pokemon. It is $height meters tall, and weighs $weight kilos.')
+TYPES = []
+
+NAME = str(QUICK_LOOK_UP.species).capitalize()
+HEIGHT = '{0:.2f}'.format(QUICK_LOOK_UP.height/10)
+WEIGHT = '{0:.2f}'.format(QUICK_LOOK_UP.weight/10)
+RAW_TYPES = QUICK_LOOK_UP.types
+
+# for type in RAW_TYPES:
+#  TEMP_TYPES.append(str(type))
+
+# FORMATTED_TYPES = ', and'.join(TEMP_TYPES)
+
+FORMATTED_VITALS = NAME_AND_VITALS.substitute(name=NAME, height=HEIGHT, weight=WEIGHT)
 
 # TO-DO:
 # Simple goal: Get height & weight in standard metric, type(s), and game(s) it has appeared in
 # Stretch goal: Return weaknesses and strengths based on type. This is less hard than tedious.
 
-print(FORMATTED)
+print(FORMATTED_VITALS)
+# print(FORMATTED_TYPES)
