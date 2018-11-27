2cccc5f21009e82c3112b876b294253cc2a42dda
Marty Hernandez Avedon <martyavedon@gmail.com>
2018-11-27 02:04:07 -0500

    GET & template string added

--- whos_that_pokemon.py
+++ whos_that_pokemon.py
@@ -6,11 +6,19 @@ whos_that_pokemon.py
 A script that makes requests against the Pokemon API.
 """
 
+from string import Template
 import argparse
 import pokebase as pb
 
-parser = argparse.ArgumentParser()
-parser.add_argument("name")
-args = parser.parse_args()
+PARSER = argparse.ArgumentParser()
+PARSER.add_argument('name', help='The name of the Pokemon you want information on.')
+ARGS = PARSER.parse_args()
+QUICK_LOOK_UP = pb.pokemon(ARGS.name)
+TEMPLATE = Template('$name, a very special Pokemon.')
+FORMATTED = TEMPLATE.substitute(name=QUICK_LOOK_UP.species[0].upper() + QUICK_LOOK_UP.species[1:])
 
-print(args.name)
+# TO-DO:
+# Simple goal: Get height & weight in standard metric, type(s), and game(s) it has appeared in
+# Stretch goal: Return weaknesses and strengths based on type. This is less hard than tedious.
+
+print(FORMATTED)
