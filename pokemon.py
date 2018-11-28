29c6bd1ec1bde27798369c6cc70c4a70e00ec579
Marty Hernandez Avedon <martyavedon@gmail.com>
2018-11-28 03:37:20 -0500

    rudimentary type matchups

--- pokemon.py
+++ pokemon.py
@@ -1,4 +1,5 @@
 from string import Template
+from type_matcher import Type_Matcher
 import json
 
 class Pokemon:
@@ -10,10 +11,14 @@ class Pokemon:
         self.weight = '{0:.2f}'.format(api_response.weight/10)
         self.types = json.loads(str(api_response.types).replace('\'', '"'))
         self.games = json.loads(str(api_response.game_indices).replace('\'', '"'))
+        self.matcher = Type_Matcher(self.types[0]['type']['name'])
+        self.strengths = self.matcher.list_strength()
+        self.weaknesses = self.matcher.list_weakness()
   
         self.name_and_measurements = self.format_name_and_measurements_string()
         self.type_description = self.format_types_string()
         self.game_appearances = self.format_games_string()
+        self.strengths_and_weaknesses = self.format_type_matches_string()
 
     def format_name_and_measurements_string(self):
         """Returns a string detailing, in plain English, the Pokemon's name, height, and weight"""
@@ -55,5 +60,38 @@ class Pokemon:
                         return_text += '\n'
 
                     return_text += '* ' + self.games[i]['version']['name'].capitalize() + '\n'
+
+        return return_text
+
+    def format_type_matches_string(self):
+        return_text = ''
+
+        if len(self.strengths) < 1:
+            return_text += 'It is not strong against anything.'
+
+        for i in range(0, len(self.strengths)):
+            if len(self.strengths) < 2:
+                return_text += 'It is strong against ' + self.strengths[i].capitalize() + '. '
+            else:
+                if i == 0:
+                    return_text += 'It is strong against ' + self.strengths[i].capitalize() + ', '
+                elif i < len(self.strengths):
+                    return_text += self.strengths[i].capitalize() + ', '
+                else:
+                    return_text += 'and ' + self.strengths[i].capitalize() + '. '
+        
+        if len(self.weaknesses) < 1:
+            return_text += 'It is not weak against anything.'
+
+        for i in range(0, len(self.weaknesses)):
+            if len(self.weaknesses) < 2:
+                return_text += 'It is weak against ' + self.weaknesses[i].capitalize() + '.'
+            else:
+                if i == 0:
+                    return_text += 'It is weak against ' + self.weaknesses[i].capitalize() + ', '
+                elif i < len(self.weaknesses):
+                    return_text += 'and ' + self.weaknesses[i].capitalize() + ', '
+                else:
+                    return_text += 'and ' + self.weaknesses[i].capitalize() + '. '
 
         return return_text
---
+++ type_matcher.py
@@ -0,0 +1,23 @@
+class Type_Matcher:
+    def __init__(self, pokemon_type):
+        self.pokemon_type = pokemon_type
+        self.strong_against = self.list_strength()
+        self.weak_against = self.list_weakness()
+
+    def list_weakness(self):
+        return {
+            'fire': ['water', 'ground'],
+            'water': ['grass', 'electric'],
+            'grass': ['fire'],
+            'ground': ['water', 'grass'],
+            'electric': ['ground']
+        }[self.pokemon_type]
+
+    def list_strength(self):
+        return {
+            'fire': ['grass'],
+            'water': ['ground'],
+            'grass': ['water', 'ground'],
+            'ground': ['fire', 'electric'],
+            'electric': ['water']
+        }[self.pokemon_type]
